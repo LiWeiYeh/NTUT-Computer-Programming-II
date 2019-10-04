1c2e7e8cda9999a2bab536c3408678d6ff928242
LiWeiYeh <liwei_yeh@hotmail.com>
2019-10-04 16:50:05 +0800

    added hw chap12.2b, renamed hw chap12.2 to chap12.2a

--- Computer-Programming-II/Computer-Programming-II/Main.c
+++ Computer-Programming-II/Computer-Programming-II/Main.c
@@ -7,7 +7,7 @@
 /* Change History: log the change history of the program         */
 /*****************************************************************/
 
-// palindrome
+// palindrome using pointers
 
 #define _CRT_SECURE_NO_WARNINGS
 
@@ -19,14 +19,14 @@
 int main(void)
 {
 	char msg[MAX_MSG_LENGTH], ch;
-	// counters
-	int i, n = 0;
-
+	//
+	char *p;
+	char *q = &msg[0];
 	// asks the user to enter a message
 	printf("Enter a message : ");
 
 	// goes through the loop
-	while (n < MAX_MSG_LENGTH) {
+	while (q < &msg[MAX_MSG_LENGTH]) {
 		// getchar() gets the user message and stores it in ch
 		// the newline will automatically be created if the user presses the "enter" key
 		// if the char is equal to a newline, break.
@@ -35,31 +35,29 @@ int main(void)
 		}
 		// if the character is alphabetical
 		if (isalpha(ch)) {
-			// makes ch uppercase and stores it into msg with index n
-			msg[n] = toupper(ch);
-			// increases the index
-			n++;
+			// makes ch uppercase and stores it into msg with *q, starting from msg[0]
+			*q = toupper(ch);
+			// appends the q, increasing the index of message by 1
+			q++;
 		}
 	}
 
-	// checks whether the first character index i is equal to the last index n - 1
+	// checks whether the first character of msg msg[0] is equal to the last character of message q - 1
 	// if equal, increase the first index and decrease last index by 1
-	for (i = 0; i < n / 2; i++) {
+	for (p = &msg[0], q--; p < q; p++, q--) {
 		// if the characterss on the indexes aren't equal
-		if (msg[i] != msg[n - i - 1]) {
-			// break
+		if (*p != *q) {
 			break;
 		}
 	}
 
 	// if the program in the for loop hasn't breaked, so
-	// if the i counter is equal to the size of the string divided by 2
-	if (i == n / 2) {
+	// if the first halve of the sentence is the same as the last halve of the sentence
+	if (p >= q) {
 		// print that it's a palindrome
 		printf("The message is a palindrome\n");
 	}
-	// if the program has breaked, so
-	// if the counter i has breaked meaning the characters weren't the same
+	// else
 	else {
 		// print that it's not a palindrome
 		printf("The message is NOT a palindrome\n");
