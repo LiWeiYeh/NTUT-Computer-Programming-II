8972c5a2ab9fc51d1663c1c95c99ab7dfb6e045c
LiWeiYeh <liwei_yeh@hotmail.com>
2019-10-04 16:23:24 +0800

    added hw chap12.2 and added gitignore file

--- Computer-Programming-II/Computer-Programming-II/Main.c
+++ Computer-Programming-II/Computer-Programming-II/Main.c
@@ -1,135 +1,67 @@
 /*****************************************************************/
 /* Class: Computer Programming II, Fall 2019                     */
-/* Author: Li-Wei Yeh (put your name here)                       */
-/* ID: 108012047 (your student ID here)                          */
-/* Date: 2019.09.22 (put program development started date here   */
-/* Purpose: finds the closest departure time flight				 */
+/* Author: Li-Wei Yeh			                                 */
+/* ID: 108012047						                         */
+/* Date: 2019.10.04											     */
+/* Purpose: description of program function                      */
 /* Change History: log the change history of the program         */
 /*****************************************************************/
 
-// flight
+// palindrome
 
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include <ctype.h>
 
-void find_closest_flight(int desired_time, int *departure_time, int *arrival_time)
-{
-	const int MAX_MINUTES_IN_A_DAY = 1440;
-
-	// these numbers are in minutes
-	int departures[] = { 480, 583, 679, 767, 840, 945, 1140, 1305 };
-	int arrivals[] = { 616, 712, 811, 900, 968, 1075, 1280, 1438 };
-	// size of array departures array constant
-	const int SIZE_OF_DEPARTURES_ARRAY = sizeof(departures) / sizeof(departures[0]) - 1;
-	// counter for finding the closest index of the array
-	int closest;
-
-
-	// if desired time is closer to first item in array, choose first item
-	if (desired_time < departures[0]) {
-		if ((departures[0] - desired_time) < (MAX_MINUTES_IN_A_DAY - departures[SIZE_OF_DEPARTURES_ARRAY] + desired_time)) {
-			closest = 0;
-		}
-		else {
-			closest = SIZE_OF_DEPARTURES_ARRAY;
-		}
-	}
-	// if desired time is closer to last item item, choose last item
-	else if (desired_time > departures[SIZE_OF_DEPARTURES_ARRAY]) {
-		closest = SIZE_OF_DEPARTURES_ARRAY;
-	}
-	// find closest item in the middle part of the array
-	else {
-		closest = 0;
-		// while the desired time is later than the closest departure time at this point, go to the next index
-		while (desired_time > departures[closest]) {
-			closest++;
-		}
-		// if previous item in array is closer, change closest to index of the array item
-		if (departures[closest] - desired_time > desired_time - departures[closest - 1]) {
-			closest = closest - 1;
-		}
-	}
-
-	// assign the values to the departure time and arrival time
-	*departure_time = departures[closest];
-	*arrival_time = arrivals[closest];
-}
-
-void print_departure_and_arrival_time(int departure_time, int arrival_time, int HOURS_IN_MINUTES, int HOURS_IN_HALF_DAY) {
-	if (departure_time / HOURS_IN_MINUTES > HOURS_IN_HALF_DAY) {
-		// if it's between 12 and 13, add pm
-		if (departure_time / HOURS_IN_MINUTES < 13) {
-			printf("Closest departure time is %d:%.2d p.m.", (departure_time / HOURS_IN_MINUTES), (departure_time % HOURS_IN_MINUTES));
-		}
-		// else, subtract 12 hours and add pm
-		else {
-			printf("Closest departure time is %d:%.2d p.m.", ((departure_time / HOURS_IN_MINUTES) - 12), (departure_time % HOURS_IN_MINUTES));
-
-		}
-	}
-	else {
-		// if it's between 0 and 1, add 12 and add am
-		if (departure_time / HOURS_IN_MINUTES < 1) {
-			printf("Closest departure time is %d:%.2d a.m.", ((departure_time / HOURS_IN_MINUTES) + 12), (departure_time % HOURS_IN_MINUTES));
-		}
-		// else, add am
-		else {
-			printf("Closest departure time is %d:%.2d a.m.", (departure_time / HOURS_IN_MINUTES), (departure_time % HOURS_IN_MINUTES));
-		}
-	}
-
-	// formatting
-	printf("\n");
-
-	// prints closest arrival time in a.m. or p.m.
-	if (arrival_time / HOURS_IN_MINUTES > HOURS_IN_HALF_DAY) {
-		// if it's between 12 and 13, add pm
-		if (arrival_time / HOURS_IN_MINUTES < 13) {
-			printf("Closest arrival time is %d:%.2d p.m.", (arrival_time / HOURS_IN_MINUTES), (arrival_time % HOURS_IN_MINUTES));
-		}
-		// else, subtract 12 and add pm
-		else {
-			printf("Closest arrival time is %d:%.2d p.m.", ((arrival_time / HOURS_IN_MINUTES) - 12), (arrival_time % HOURS_IN_MINUTES));
-		}
-	}
-	else {
-		// if it's between 0 and 1, add 12 and add am
-		if (arrival_time / HOURS_IN_MINUTES < 1) {
-			printf("Closest arrival time is %d:%.2d a.m.", ((arrival_time / HOURS_IN_MINUTES) + 12), (arrival_time % HOURS_IN_MINUTES));
-		}
-		// else, add am
-		else {
-			printf("Closest arrival time is %d:%.2d a.m.", (arrival_time / HOURS_IN_MINUTES), (arrival_time % HOURS_IN_MINUTES));
-		}
-	}
-}
+#define MAX_MSG_LENGTH 80
 
 int main(void)
 {
-	printf("Nearest flight calculator...\n");
+	char msg[MAX_MSG_LENGTH], ch;
+	// counters
+	int i, n = 0;
 
-	// Constant values
-	const int HOURS_IN_MINUTES = 60;
-	const int HOURS_IN_HALF_DAY = 12;
+	// asks the user to enter a message
+	printf("Enter a message : ");
 
-	int hours, minutes;
-	int desired_time, departure_time, arrival_time;
+	// goes through the loop
+	while (n < MAX_MSG_LENGTH) {
+		// getchar() gets the user message and stores it in ch
+		// the newline will automatically be created if the user presses the "enter" key
+		// if the char is equal to a newline, break.
+		if ((ch = getchar()) == '\n') {
+			break;
+		}
+		// if the character is alphabetical
+		if (isalpha(ch)) {
+			// makes ch uppercase and stores it into msg with index n
+			msg[n] = toupper(ch);
+			// increases the index
+			n++;
+		}
+	}
 
+	// checks whether the first character index i is equal to the last index n - 1
+	// if equal, increase the first index and decrease last index by 1
+	for (i = 0; i < n / 2; i++) {
+		// if the characterss on the indexes aren't equal
+		if (msg[i] != msg[n - i - 1]) {
+			// break
+			break;
+		}
+	}
 
-
-	// Asks the user for a 24-hour time input in hh:mm format
-	printf("Enter a 24-hour time: ");
-	scanf("%d:%d", &hours, &minutes);
-
-	desired_time = hours * HOURS_IN_MINUTES + minutes;
-
-	// function call to find the closest flight
-	find_closest_flight(desired_time, &departure_time, &arrival_time);
-
-	// Prints closest departure time in a.m. or p.m.
-	print_departure_and_arrival_time(departure_time, arrival_time, HOURS_IN_MINUTES, HOURS_IN_HALF_DAY);
-
-	return 0;
+	// if the program in the for loop hasn't breaked, so
+	// if the i counter is equal to the size of the string divided by 2
+	if (i == n / 2) {
+		// print that it's a palindrome
+		printf("The message is a palindrome\n");
+	}
+	// if the program has breaked, so
+	// if the counter i has breaked meaning the characters weren't the same
+	else {
+		// print that it's not a palindrome
+		printf("The message is NOT a palindrome\n");
+	}
 }
--- hw04_108012047/chap12_project02/chap12_project02.c
+++ hw04_108012047/chap12_project02/chap12_project02.c
@@ -1,9 +1,67 @@
-/*****************************************************************/
-/* Class: Computer Programming II, Fall 2019                     */
-/* Author: xxx (put your name here)                              */
-/* ID: 107990xxx (your student ID here)                          */
-/* Date: 2019.03.07 (put program development started date here   */
-/* Purpose: description of program function                      */
-/* Change History: log the change history of the program         */
-/*****************************************************************/
-
+///*****************************************************************/
+///* Class: Computer Programming II, Fall 2019                     */
+///* Author: Li-Wei Yeh			                                 */
+///* ID: 108012047						                         */
+///* Date: 2019.10.04											     */
+///* Purpose: Checks whether an entered message is a palindrome	 */
+///* Change History: log the change history of the program         */
+///*****************************************************************/
+//
+//// palindrome
+//
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//#include <ctype.h>
+//
+//#define MAX_MSG_LENGTH 80
+//
+//int main(void)
+//{
+//	char msg[MAX_MSG_LENGTH], ch;
+//	// counters
+//	int i, n = 0;
+//
+//	// asks the user to enter a message
+//	printf("Enter a message : ");
+//
+//	// goes through the loop
+//	while (n < MAX_MSG_LENGTH) {
+//		// getchar() gets the user message and stores it in ch
+//		// the newline will automatically be created if the user presses the "enter" key
+//		// if the char is equal to a newline, break.
+//		if ((ch = getchar()) == '\n') {
+//			break;
+//		}
+//		// if the character is alphabetical
+//		if (isalpha(ch)) {
+//			// makes ch uppercase and stores it into msg with index n
+//			msg[n] = toupper(ch);
+//			// increases the index
+//			n++;
+//		}
+//	}
+//
+//	// checks whether the first character index i is equal to the last index n - 1
+//	// if equal, increase the first index and decrease last index by 1
+//	for (i = 0; i < n / 2; i++) {
+//		// if the characterss on the indexes aren't equal
+//		if (msg[i] != msg[n - i - 1]) {
+//			// break
+//			break;
+//		}
+//	}
+//
+//	// if the program in the for loop hasn't breaked, so
+//	// if the i counter is equal to the size of the string divided by 2
+//	if (i == n / 2) {
+//		// print that it's a palindrome
+//		printf("The message is a palindrome\n");
+//	}
+//	// if the program has breaked, so
+//	// if the counter i has breaked meaning the characters weren't the same
+//	else {
+//		// print that it's not a palindrome
+//		printf("The message is NOT a palindrome\n");
+//	}
+//}
