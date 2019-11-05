fd4353f4cd306277fca511cab1a516c46d14ce40
LiWeiYeh <liwei_yeh@hotmail.com>
2019-11-05 20:17:20 +0800

    commented everything

--- Computer-Programming-II/Computer-Programming-II/Main.c
+++ Computer-Programming-II/Computer-Programming-II/Main.c
@@ -1,28 +1,10 @@
-/*****************************************************************/
-/* Class: Computer Programming II, Fall 2019                     */
-/* Author: Li-Wei Yeh						                     */
-/* ID: 108012047						                         */
-/* Date: 2019.10.16												 */
-/* Purpose: Sums the entered elements							 */
-/* Change History:												 */
-/*****************************************************************/
-
-// sums the entered elements
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char * argv[])
+int main()
 {
-	printf("Program to sum the entered elements\n");
-
-	int i, sum = 0;
-
-	for (i = 1; i < argc; i++) {
-		sum += atoi(argv[i]);
-	}
-
-	printf("total: %d\n", sum);
+	printf("Hello world");
 
 	return 0;
 }
--- hw04_108012047/chap13_project05/chap13_project05.c
+++ hw04_108012047/chap13_project05/chap13_project05.c
@@ -1,28 +1,28 @@
-/*****************************************************************/
-/* Class: Computer Programming II, Fall 2019                     */
-/* Author: Li-Wei Yeh						                     */
-/* ID: 108012047						                         */
-/* Date: 2019.10.16												 */
-/* Purpose: Sums the entered elements from the command line		 */
-/* Change History:												 */
-/*****************************************************************/
-
-// sums the entered elements
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char * argv[])
-{
-	printf("Program to sum the entered elements\n");
-
-	int i, sum = 0;
-
-	for (i = 1; i < argc; i++) {
-		sum += atoi(argv[i]);
-	}
-
-	printf("total: %d\n", sum);
-
-	return 0;
-}
+///*****************************************************************/
+///* Class: Computer Programming II, Fall 2019                     */
+///* Author: Li-Wei Yeh						                     */
+///* ID: 108012047						                         */
+///* Date: 2019.10.16												 */
+///* Purpose: Sums the entered elements from the command line		 */
+///* Change History:												 */
+///*****************************************************************/
+//
+//// sums the entered elements
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(int argc, char * argv[])
+//{
+//	printf("Program to sum the entered elements\n");
+//
+//	int i, sum = 0;
+//
+//	for (i = 1; i < argc; i++) {
+//		sum += atoi(argv[i]);
+//	}
+//
+//	printf("total: %d\n", sum);
+//
+//	return 0;
+//}
