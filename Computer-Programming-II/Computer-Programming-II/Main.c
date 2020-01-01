e6622fd67c8a6c566b2618b9882f1ab0448e01ab
LiWeiYeh <liwei_yeh@hotmail.com>
2020-01-01 23:18:15 +0800

    cleaned up everything

--- Computer-Programming-II/Computer-Programming-II/Main.c
+++ Computer-Programming-II/Computer-Programming-II/Main.c
@@ -7,33 +7,13 @@
 /* Change History: log the change history of the program         */
 /*****************************************************************/
 
-//quicksort
 
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include "quicksort.h"
-
-#define N 10
 
 int main()
 {
-
-	int a[N], i;
-
-	printf("Enter %d numbers to be sorted: ", N);
-	for (i = 0; i < N; i++) {
-		scanf("%d", &a[i]);
-	}
-	
-	quicksort(a, 0, N - 1);
-
-	printf("In sorted order: ");
-	for (i = 0; i < N; i++) {
-		printf("%d", a[i]);
-	}
-	printf("\n");
-
 	return 0;
 }
